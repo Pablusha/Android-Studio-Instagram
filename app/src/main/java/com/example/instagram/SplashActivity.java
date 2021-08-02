38a46fe6fffa3a5f1fddd0c60f40a5191d5ed7b2
Yasin <yasin_akdogan15@hotmail.com>
2021-08-03 00:31:15 +0300

    Splash Activity tamamlandı.

--- app/src/main/java/com/example/instagram/SplashActivity.java
+++ app/src/main/java/com/example/instagram/SplashActivity.java
@@ -2,15 +2,47 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView txtFacebook;
+    private int GECIS_SURESI = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        
+        txtFacebook = findViewById(R.id.ac_splash_tvFacebook);
+        changeColorOfFacebookText();
+        toTheLoginScreen();
+
     }
+
+    private void changeColorOfFacebookText() {
+        txtFacebook.setText(Html.fromHtml("<font color=\"#fa8706\">" + "F" +
+                "</font>" + "<font color=\"#f26906\">" + "A" + "</font>" +
+                "<font color=\"#e64302\">" + "C" + "</font>" +
+                "<font color=\"#eb1504\">" + "E" + "</font>" +
+                "<font color=\"#da0111\">" + "B" + "</font>" +
+                "<font color=\"#cf012d\">" + "O" + "</font>" +
+                "<font color=\"#c80251\">" + "O" + "</font>" +
+                "<font color=\"#b40375\">" + "K" + "</font>"));
+    }
+
+    private void toTheLoginScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },GECIS_SURESI);
+    }
+
 }
