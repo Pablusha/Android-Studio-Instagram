422e14e43d980124e392c3f627291a45886a419f
Yasin <yasin_akdogan15@hotmail.com>
2021-08-07 20:15:11 +0300

    First Register Screen's design completed.

--- app/src/main/java/com/example/instagram/RegisterScreen.java
+++ app/src/main/java/com/example/instagram/RegisterScreen.java
@@ -4,13 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterScreen extends AppCompatActivity {
+
+    private TextView txtGirisYap;
+    private Button btnFacebook,btnKaydol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        initViews();
+
+        goToLogin();
+        goToSecondRegisterScreen();
+
+    }
+
+    private void initViews() {
+        txtGirisYap = findViewById(R.id.ac_register_txtGirisYap);
+        btnFacebook = findViewById(R.id.ac_register_btnGirisWithFacebook);
+        btnKaydol = findViewById(R.id.ac_register_btnEmailOrPhoneNumber);
+    }
+
+    private void goToLogin() {
+        txtGirisYap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterScreen.this,LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void goToSecondRegisterScreen() {
+        btnKaydol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterScreen.this,SecondRegisterScreen.class));
+                finish();
+            }
+        });
     }
 
     @Override
