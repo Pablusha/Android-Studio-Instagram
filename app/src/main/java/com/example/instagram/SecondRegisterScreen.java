f74584497cf1f56efbc98062f2a69e7ccf2ecb2a
Yasin <yasin_akdogan15@hotmail.com>
2021-08-30 19:11:42 +0300

    User Registrationda sıkıntı var hala çözmeye çalışıyorum.

--- app/src/main/java/com/example/instagram/SecondRegisterScreen.java
+++ app/src/main/java/com/example/instagram/SecondRegisterScreen.java
@@ -1,5 +1,6 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -13,10 +14,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Locale;
+
 public class SecondRegisterScreen extends AppCompatActivity {
 
-    private EditText etUsername,etFullName,etEmail,etPassword,etConfirmPassword;
-    private Button btnKayit;
+    EditText etUsername,etFullName,etEmail,etPassword,etConfirmPassword;
+    Button btnKayit;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,38 +40,71 @@ public class SecondRegisterScreen extends AppCompatActivity {
         initViews();
         changeButtonBackground();
 
-        register();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-    }
-
-    private void register() {
         btnKayit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = etUsername.getText().toString();
-                String fullName = etFullName.getText().toString();
-                String email = etEmail.getText().toString();
-                String password = etPassword.getText().toString();
-                String confPassword = etConfirmPassword.getText().toString();
-
-                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(fullName) || TextUtils.isEmpty(email)
-                        || TextUtils.isEmpty(password) || TextUtils.isEmpty(confPassword)) {
-                    Toast.makeText(SecondRegisterScreen.this,"Alanlar boş bırakılamaz.",
-                            Toast.LENGTH_SHORT).show();
-                }
-
-                if (!TextUtils.equals(password,confPassword)) {
-                    Toast.makeText(SecondRegisterScreen.this,"Şifreleriniz uyuşmuyor.",Toast.LENGTH_SHORT).show();
-                }
-
-                
-
-
-
-
+                register();
             }
         });
 
+    }
+
+    public void register() {
+        String username = etUsername.getText().toString();
+        String fullName = etFullName.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String confPassword = etConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(fullName) || TextUtils.isEmpty(email)
+                || TextUtils.isEmpty(password) || TextUtils.isEmpty(confPassword)) {
+            Toast.makeText(SecondRegisterScreen.this,"Alanlar boş bırakılamaz.",
+                    Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 6) {
+            Toast.makeText(SecondRegisterScreen.this,"Şifrenizin uzunluğu 6 karakterden " +
+                    "fazla olmalıdır.",Toast.LENGTH_LONG).show();
+        } else if (!TextUtils.equals(password,confPassword)) {
+            Toast.makeText(SecondRegisterScreen.this,"Şifreleriniz uyuşmuyor.",Toast.LENGTH_SHORT).show();
+        } else {
+            createUser(username,fullName,email,password);
+        }
+    }
+
+    public void createUser(String username,String fullName,String email,String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            String userId = firebaseUser.getUid();
+
+                            databaseReference = FirebaseDatabase.getInstance().getReference().child("Users")
+                                    .child(userId);
+
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("id",userId);
+                            hashMap.put("username",username.toLowerCase(Locale.ROOT));
+                            hashMap.put("fullName",fullName);
+                            hashMap.put("bio","");
+
+                            databaseReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(SecondRegisterScreen.this,LoginActivity.class));
+                                        finish();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(SecondRegisterScreen.this,task.getException().toString(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     private void changeButtonBackground() {
@@ -98,7 +146,7 @@ public class SecondRegisterScreen extends AppCompatActivity {
         }
     };
 
-    private void initViews() {
+    public void initViews() {
         etUsername = findViewById(R.id.ac_secondRegister_etUsername);
         etFullName = findViewById(R.id.ac_secondRegister_etFullName);
         etEmail = findViewById(R.id.ac_secondRegister_etEmail);
